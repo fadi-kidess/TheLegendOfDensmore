9265c79267fc32acb038f1f6d1aa9bd372ba626c
diego21001 <diegodc@bu.edu>
2022-12-09 14:10:32 -0500

    debugging

--- TheLegendOfDensmore/Assets/Code/Diego_Battles/Battling.cs
+++ TheLegendOfDensmore/Assets/Code/Diego_Battles/Battling.cs
@@ -411,12 +411,12 @@ public class Battling : MonoBehaviour //must add break statements to the multi-e
                         return;
                     }
                 }
-                if(Enemy1.activeSelf == false)
-                {
-                    choice.GetComponent<TextChoice>().deactivate();
-                }
+                // if(Enemy1.activeSelf == false)
+                // {
+                //     choice.GetComponent<TextChoice>().deactivate();
+                // }
         
-                break;
+                // break;
             }
         }
         //Enemy1.GetComponent<Enemy>().enemy_health
